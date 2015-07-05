18df77e91d464309589c081a5854143cd6908719
Shenouda-Fawzy <Shenouda.Fawzy@hotmail.com>
2015-07-05 17:47:45 +0200

    Solving converting the char 2

--- src/Farabco_Package/Farabco.java
+++ src/Farabco_Package/Farabco.java
@@ -218,7 +218,7 @@ public class Farabco {
         francoKey.put("8" , 'غ');
 	francoKey.put("f" , 'ف');
 	francoKey.put("F" , 'ف');
-	francoKey.put("2" , 'ق');
+	francoKey.put("2" , 'أ');
 	
 	francoKey.put("k" , 'ك');
 	francoKey.put("K" , 'ك');
